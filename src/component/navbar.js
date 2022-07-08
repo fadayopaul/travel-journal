1f603d8bb49e7e945e4bcf2c56be698706479dd9
fadayopaul <paulfadayo@yahoo.com>
2022-07-08 18:30:21 +0100

    Updated

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -3,8 +3,10 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="nav--body">
-      <img src={require("../images/logo.png")} className="logo" alt="logo" />
-      <p className="logo--text">my travel journal</p>
+      <div className="nav-bar-items">
+        <img src={require("../images/logo.gif")} className="logo" alt="logo" />
+        <p className="logo--text">my travel journal</p>
+      </div>
     </nav>
   );
 }
